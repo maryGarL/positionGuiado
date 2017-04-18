dbe919dc0e7a684f12ecb766effa1a8f4e4c3a93
Mariel Garcia <maryel_565@hotmail.com>
2017-04-18 16:22:04 -0500

    desafio 5

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+var contenedor=document.getElementsByClassName("contenedor")[0];
+var azul=document.getElementById("azul");
+var rosa=document.getElementById("rosa");
+var verde=document.getElementById("verde");
+var amarillo=document.getElementById("amarillo");
+
+contenedor.style.position="relative";
+contenedor.style.background="DarkGray";
+contenedor.style.width="600px";
+contenedor.style.padding="50px";
+azul.style.position="absolute";
+azul.style.top="250px";
+azul.style.right="0";
